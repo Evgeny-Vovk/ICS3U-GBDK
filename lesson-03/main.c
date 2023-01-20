87a4a27193a8b03362f082ad15a3a9ec112ed47d
Evgeny Vovk <evgeny.vovk@mths.ca>
2023-01-20 16:16:44 +0000

    done lesson 3

---
+++ lesson-03/main.c
@@ -0,0 +1,33 @@
+// Copyright (c) 2022 Evgeny Vovk All rights reserved.
+//
+// Created by: Evgeny Vovk
+// Created on: December 2022
+// This program is the Space Aliens for the GameBoy
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+#include "MetaSprites.c"
+#include "SpaceAliens.c"
+#include "Background.c"
+
+void main () {
+    set_sprite_data(0, 16, SpaceAliens);
+    set_meta_sprite_tile(0, 1, 2, 3, 4);
+    move_meta_sprite(0, 76, 144);
+
+    set_bkg_data(0, 16, SpaceAliens);
+    set_bkg_tiles(0, 0, 20, 36, Background);
+
+    SHOW_BKG;
+    SHOW_SPRITES;
+    DISPLAY_ON;
+
+    // game loop
+    while (1) {
+        scroll_bkg(0, -1);
+
+        wait_vbl_done();
+    }
+}
+
