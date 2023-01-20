33d4e444fe4419c5de08946ceef4cc617d529b0d
Evgeny Vovk <evgeny.vovk@mths.ca>
2023-01-20 15:15:27 +0000

    all new files

---
+++ lesson-01/main.c
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Evgeny Vovk All rights reserved.
+//
+// Created by: Evgeny Vovk
+// Created on: December 2022
+// This program is the Space Aliens for the GameBoy
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main () {
+    printf("Hello, World!");
+}
+
